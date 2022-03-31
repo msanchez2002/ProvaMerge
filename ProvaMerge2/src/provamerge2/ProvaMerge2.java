fba97f355af1160bfdc2cf26bf147b383f33f30b
msanchez2002 <101583431+msanchez2002@users.noreply.github.com>
2022-03-31 18:57:20 +0200

    Add files via upload

---
+++ ProvaMerge2/src/provamerge2/ProvaMerge2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provamerge2;
+
+/**
+ *
+ * @author marck
+ */
+public class ProvaMerge2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("A");
+        
+        System.out.println("B");
+        
+        System.out.println("C");
+        
+    }
+    
+}
